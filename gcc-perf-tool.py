1c55fcd5843e690bfe6d8c6f68cec4e3a2d52b9d
Markel Mencía <markel.mnc@gmail.com>
2026-02-23 20:11:19 +0100

    Add program options

--- gcc-perf-tool.py
+++ gcc-perf-tool.py
@@ -1,8 +1,6 @@
 import sys
 import subprocess
 
-instrumentalize = True
-
 def perf_profile(output_bin):
     print("Profiling...")
     PERF_COMMAND = ["perf", "stat", "./" + output_bin]
@@ -17,20 +15,39 @@ def perf_profile(output_bin):
     print()
 
 def main():
-    if len(sys.argv) <= 1:
+
+    source_file = ""
+    program_options = []
+    gcc_options = []
+
+    for i in range(1, len(sys.argv)):
+        if "-" not in sys.argv[i]:
+            source_file = sys.argv[i]
+            gcc_options = sys.argv[i+1:]
+            break
+
+        program_options.append(sys.argv[i])
+
+    if source_file == "":
         print("gcc-perf-tool: missing source file")
         sys.exit(1)
 
-    SOURCE_FILE = sys.argv[1]
-
-    if len(sys.argv) > 1:
-        COMPILER_FLAGS = sys.argv[2:len(sys.argv)]
+    # Program options
+    instrumentalize = "-i" in program_options
+    
+    output_bin = source_file + ".out"
+    for option in program_options:
+        if option.startswith("-output="):
+            output_bin = option.strip().split("=")[1]
+            if output_bin == "":
+                print("gcc-perf-tool: missing output binary name")
+                sys.exit(-1)
 
 
     # Binary compilation
     print("Compiling binary...")
-    OUTPUT_BIN = SOURCE_FILE + ".out"
-    GCC_COMMAND = ["gcc"] + COMPILER_FLAGS + [SOURCE_FILE] + ["-o"] + [OUTPUT_BIN]
+    
+    GCC_COMMAND = ["gcc"] + gcc_options + [source_file] + ["-o"] + [output_bin]
 
     gcc_process = subprocess.run(GCC_COMMAND, capture_output=True)
     if gcc_process.returncode != 0:
@@ -40,11 +57,11 @@ def main():
 
     # Profiling execution
     print("Profiling...")
-    perf_profile(OUTPUT_BIN)
+    perf_profile(output_bin)
 
     if instrumentalize:
         print("Compiling with instrumentation profiler...")
-        GCC_INSTRUMENTATION_COMMAND = ["gcc"] + ["-fprofile-generate"] + COMPILER_FLAGS + [SOURCE_FILE] + ["-o"] + [OUTPUT_BIN]
+        GCC_INSTRUMENTATION_COMMAND = ["gcc"] + ["-fprofile-generate"] + gcc_options + [source_file] + ["-o"] + [output_bin]
         
         gcc_instrumentation_process = subprocess.run(GCC_INSTRUMENTATION_COMMAND, capture_output=True)
         if gcc_instrumentation_process.returncode != 0:
@@ -53,10 +70,10 @@ def main():
             sys.exit(gcc_instrumentation_process.returncode)
 
         print("Generating profiling data...")
-        subprocess.run(["./" + OUTPUT_BIN], capture_output=True)
+        subprocess.run(["./" + output_bin], capture_output=True)
 
         print("Recompiling with profiling data...")
-        GCC_RECOMPILATION_COMMAND = ["gcc"] + ["-fprofile-use"] + COMPILER_FLAGS + [SOURCE_FILE] + ["-o"] + [OUTPUT_BIN]
+        GCC_RECOMPILATION_COMMAND = ["gcc"] + ["-fprofile-use"] + gcc_options + [source_file] + ["-o"] + [output_bin]
 
         gcc_recompilation_process = subprocess.run(GCC_RECOMPILATION_COMMAND, capture_output=True)
         if gcc_recompilation_process.returncode != 0:
@@ -64,6 +81,6 @@ def main():
             print(gcc_recompilation_process.stderr.decode().strip())
             sys.exit(gcc_recompilation_process.returncode)
 
-        perf_profile(OUTPUT_BIN)
+        perf_profile(output_bin)
 
 main()
